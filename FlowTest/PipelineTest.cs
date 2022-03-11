8b500edcdc02273f0f81500ef0b95410cb1b4320
Maxim Konstantinovski <vbwizardry@gmail.com>
2022-03-11 10:43:31 -0500

    migrated to new framework and added test project

---
+++ FlowTest/PipelineTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FlowTest
+{
+    [TestClass]
+    public class PipelineTest
+    {
+        [TestMethod]
+        public void RunTest()
+        {
+        }
+    }
+}
